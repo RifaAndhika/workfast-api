b60b44a4e37cd4b37f68978859da3087ec312799
RifaAndhika <rifaandhika9@gmail.com>
2026-08-21 23:58:17 +0700

    feat: initialize Redis connection and add event handlers for connection status

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,6 +1,15 @@
 import { Redis } from "ioredis";
 import dotenv from "dotenv";
 
+dotenv.config();
 export const connection = new Redis(process.env.REDIS_URL!, {
   maxRetriesPerRequest: null,
+});
+
+connection.on("connect", () => {
+  console.log("🚀 Sukses: Terhubung ke Upstash Redis!");
+});
+
+connection.on("error", (err) => {
+  console.error("❌ Gagal konek ke Redis:", err.message);
 });
