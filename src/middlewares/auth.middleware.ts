8ee282430159c653bdebbb0fec7d2605f805cd21
RifaAndhika <rifaandhika9@gmail.com>
2026-08-23 22:28:56 +0700

    feat: implement authentication middleware and token verification logic

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,38 @@
+import { Request, Response, NextFunction } from "express";
+import { verifyToken } from "../modules/auth/auth.utils";
+
+export function authMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const authorizationHeader = req.headers.authorization;
+
+  if (!authorizationHeader) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const parts = authorizationHeader.split(" ");
+  // 2. Perbaikan logika: Error jika BUKAN Bearer atau format salah
+  if (parts.length !== 2 || parts[0] !== "Bearer") {
+    return res
+      .status(401)
+      .json({ message: "Authorization format must be Bearer [token]" });
+  }
+
+  const token = parts[1];
+
+  // 3. Perbaikan logika: Error jika token KOSONG
+  if (!token) {
+    return res.status(401).json({ message: "Token is missing" });
+  }
+
+  const decoded = verifyToken(token);
+  // TAMBAHKAN PENGECEKAN INI:
+  if (!decoded) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+
+  req.user = decoded;
+  next();
+}
--- src/modules/auth/auth.utils.ts
+++ src/modules/auth/auth.utils.ts
@@ -1,27 +1,34 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-const SALT_ROUNDS = 10; // kenapa 10 karena standar industri dan tidak membuat server dan ux yang terlalu berat
+const SALT_ROUNDS = 10;
 
-export async function hashPassword(plainPassword: string): Promise<string> {
+export const hashPassword = async (plainPassword: string): Promise<string> => {
   return await bcrypt.hash(plainPassword, SALT_ROUNDS);
-}
+};
 
-export async function comparePassword(
+export const comparePassword = async (
   plainPassword: string,
   hashedPassword: string,
-): Promise<boolean> {
+): Promise<boolean> => {
   return await bcrypt.compare(plainPassword, hashedPassword);
-}
+};
 
 export interface JwtPayload {
-  id: string;
+  userId: string;
   email: string;
   role: string;
 }
 
-export async function generteToken(payload: JwtPayload): Promise<string> {
-  return await jwt.sign(payload, process.env.JWT_SECRET!, {
-    expiresIn: "1d",
-  });
+export function generateToken(payload: JwtPayload) {
+  return jwt.sign(payload, process.env.JWT_SECRET!, { expiresIn: "1d" });
+}
+
+export function verifyToken(token: string) {
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+    return decoded as JwtPayload;
+  } catch {
+    return null;
+  }
 }
